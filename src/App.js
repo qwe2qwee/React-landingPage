c4f05dc6423671326fd370f9b6476db54215a702
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2024-01-23 19:47:56 +0300

    working on the navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Layout>heloo</Layout>
+  
+    </div>
+  );
+}
+
+export default App;
