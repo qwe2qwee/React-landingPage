b06dc7e28ce7e7cbb1aa2b0158692f2ff1f3df8e
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2024-02-19 15:24:03 +0300

    digitalmarkting page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./Pages/Home/Home";
 import Services from "./Pages/Services/Services";
+import First from "./components/SeoCom/First";
 function App() {
   return (
     <Router>
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='Servers' element={<Services />} />
+          <Route path='DigitalMarketing' element={<First />} />
         </Routes>
       </Layout>
     </Router>
---
+++ src/Pages/SEO/Seo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Seo.css";
+import First from "../../components/SeoCom/First";
+
+const Seo = () => {
+  return <>
+  <First/>
+  </>;
+};
+
+export default Seo;
